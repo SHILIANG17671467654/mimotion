5e44b6d7cc01c78243839c4aa2835c9bc28ff3aa
SHILIANG17671467654 <35520539+SHILIANG17671467654@users.noreply.github.com>
2026-06-18 12:53:47 +0800

    Update inspect_configs.py

--- inspect_configs.py
+++ inspect_configs.py
@@ -1,10 +1,10 @@
 # -*- coding: utf8 -*-
-pip3 install requests pycryptodome pytz
 import os
 
 from util import push_util
 from util import aes_help
 
+pip3 install requests pycryptodome pytz
 
 def build_inspect_configs_content(config_param, aes_key_param, pat_param):
     if aes_key_param is None or aes_key_param == "":
